f3338a07440f484002740bf3bda60cd8849cf84f
ttaannsseell73-alt <ttaannsseell73@gmail.com>
2026-09-19 21:34:41 +0300

    Force TESTNET endpoints for SHADOW collector

--- src/tools/runShadow.ts
+++ src/tools/runShadow.ts
@@ -1,4 +1,8 @@
 process.env.QUANT_EXECUTION_MODE = 'SHADOW';
 process.env.SHADOW_CLEAN_START = 'yes';
+process.env.BINANCE_FUTURES_URL = 'https://testnet.binancefuture.com';
+process.env.BINANCE_FUTURES_WS_URL = 'wss://stream.binancefuture.com';
+process.env.I_UNDERSTAND_LIVE = 'NO';
+process.env.I_UNDERSTAND_QUANT_LIVE = 'NO';
 
 void import('../index');
